becf5fc80bbef29dab86109f04f588bf76f533d3
Chris Rimes <chris.rimes@gmail.com>
2018-03-24 23:17:12 +0000

    Hide window creation in the terminal module.
    Ask the user for their name when run.

--- chat.py
+++ chat.py
@@ -1,19 +1,13 @@
 import client as chat
-from terminal import new_window, SCREEN_HEIGHT, SCREEN_WIDTH, NEW_LINE, BOLD
+from terminal import message_log, input_box, BOLD, NEW_LINE
 
 # Actions
 HELLO = 'HELLO'
 SAY = 'SAY'
 BYE = 'BYE'
 
-my_name = 'alice'
-
 # Keep track of who's online (ip address -> name)
-people = {chat.ip_address: my_name}
-
-# divide the window into a scrolling chat log and an input box for typing messages
-log_window = new_window(SCREEN_HEIGHT - 1, SCREEN_WIDTH, 0, 0, scrolling = True)
-input_window = new_window(1, SCREEN_WIDTH, SCREEN_HEIGHT - 1, 0)
+people = {}
 
 # define a function to handle incoming messages
 def message_received(ip, action, message):
@@ -22,31 +16,33 @@ def message_received(ip, action, message):
     if not ip in people:
       name = message
       people[ip] = name
-      log_window.write(name, style = BOLD)
-      log_window.write(' joined the conversation.', NEW_LINE)
+      message_log.print(name, style = BOLD)
+      message_log.print(' joined the conversation.', NEW_LINE)
       chat.tell(ip, HELLO, my_name) # reply so that the new person knows who I am
   elif action == SAY:
     # someone said something
     name = people[ip] if ip in people else 'anonymous'
-    log_window.write('[', name, '] ', style = BOLD)
-    log_window.write(message, NEW_LINE)
+    message_log.print('[', name, '] ', style = BOLD)
+    message_log.print(message, NEW_LINE)
   elif action == BYE:
     # someone has left the conversation
     if ip in people:
       name = people.pop(ip)
-      log_window.write(name, style = BOLD)
-      log_window.write(' left the conversation.', NEW_LINE)
-  input_window.write('') # return focus to the input window
+      message_log.print(name, style = BOLD)
+      message_log.print(' left the conversation.', NEW_LINE)
 
 chat.when_message_received(message_received)
+
+my_name = input_box.ask("What's your name? ")
+my_ip = chat.ip_address
+people[my_ip] = my_name
 
 # let everyone know you're here
 chat.broadcast(HELLO, my_name)
 
 finished = False
 while not finished:
-    input_window.clear()
-    message = input_window.read(prompt = 'Write a message: ')
+    message = input_box.ask("Enter a message: ")
     if message.lower() == 'bye':
       finished = True
     else:
--- terminal.py
+++ terminal.py
@@ -1,58 +1,60 @@
 """
 Utilities for working with the terminal.
 
-Functions:
-* new_window(height, width, top, left, scrolling = True|False) - create a new window in the terminal
+Splits the terminal into two parts - a scrolling message log and a user input
+box - and provides facades for accessing the two parts.
 
-Constants:
-* SCREEN_HEIGHT - the number of lines that fit on the screen
-* SCREEN_WIDTH - the number of columns that fit on the screen
-* NORMAL - used with the write function to produce normal text
-* BOLD - used with the write function to produce bold text
-* NEW_LINE - used with the write function to start a new line
+Members:
+* message_log - the message log
+* input_box - the input box
+* NORMAL - style constant used with the write function to produce normal text
+* BOLD - style constant used with the write function to produce bold text
+* NEW_LINE - the new line character
+* MAX_LINES - constant containing the height of the terminal in lines of text
+* MAX_COLS - constant containing the width of the terminal in columns of text
 """
 import curses
 
-SCREEN_HEIGHT = curses.LINES
-SCREEN_WIDTH = curses.COLS
+MAX_LINES = curses.LINES  # pylint: disable=no-member
+MAX_COLS = curses.COLS    # pylint: disable=no-member
 
 BOLD = curses.A_BOLD
 NORMAL = curses.A_NORMAL
 
 NEW_LINE = '\n'
 
-def new_window(height, width, top, left, scrolling = False):
-    "Create a new window with the given dimensions."
-    window = curses.newwin(height, width, top, left)
-    window.immedok(True)  # update immediately
-    window.scrollok(scrolling)
-    return __wrap(window)
+__mainwindow = curses.newwin(MAX_LINES, MAX_COLS, 0, 0)
+__mainwindow.immedok(True)  # update immediately
+__mainwindow.hline(MAX_LINES - 2, 0, curses.ACS_HLINE, MAX_COLS)
 
-class Window():
-    """
-    A terminal window.
+# divide the main window into a scrolling chat log and an input box for typing messages
 
-    Operations:
-    * write(str, ..., style = NORMAL|BOLD) - write some text to a window
-    * read(prompt) - read some text from the user
-    * clear() - clear all text from the window
-    """
+_logwindow = __mainwindow.subwin(MAX_LINES - 2, MAX_COLS, 0, 0)
+_logwindow.immedok(True)  # update immediately
+_logwindow.scrollok(True)
 
-    def __init__(self, window):
-        self.__window = window
+_inputwindow = __mainwindow.subwin(1, MAX_COLS, MAX_LINES - 1, 0)
+_inputwindow.immedok(True)  # update immediately
 
-    def write(self, *objects, style = NORMAL):
-        "Write text starting at the current position of the cursor."
-        for obj in objects:
-            self.__window.addstr(obj, style)
+class MessageLog(object):
 
-    def read(self, prompt = ''):
-        "Read input from the user, with an optional prompt."
-        self.write(prompt, style = BOLD)
-        return self.__window.getstr().decode()
+  def print(self, *objects, style = NORMAL):
+    "Write text starting at the current position of the cursor."
+    for obj in objects:
+      _logwindow.addstr(obj, style)
+    _inputwindow.addstr('') # return focus to the input window
 
-    def clear(self):
-        self.__window.clear()
+  def clear(self):
+    "Clear the window."
+    _logwindow.clear()
 
-def __wrap(window):
-    return Window(window)
+class InputBox(object):
+
+  def ask(self, prompt = ''):
+    "Read input from the user, with optional prompt."
+    _inputwindow.clear()
+    _inputwindow.addstr(prompt, curses.A_BOLD)
+    return _inputwindow.getstr(MAX_COLS - len(prompt)).decode()
+
+message_log = MessageLog()
+input_box = InputBox()
